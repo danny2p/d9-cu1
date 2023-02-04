673cebb9266bc72ea64ea92e8f6a2d5ca6393509
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2023-02-03 22:58:32 -0700

    github actions to parallel deploy

---
+++ web/.github/scripts/dev-deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Usage
+# ./dev-deploy.sh <site-name or uuid>
+
+# Exit on error
+set -e
+
+SITE=$1
+DEV=$(echo "${SITE}.dev")
+START=$SECONDS
+
+# Tell slack we're starting this site
+SLACK_START="Started ${SITE} deployment"
+
+curl -X POST -H 'Content-type: application/json' --data "{'text':'${SLACK_START}'}" $SLACK_WEBHOOK
+echo -e "Starting ${SITE}";
+
+# Check site upstream for updates, apply
+terminus site:upstream:clear-cache $1 -q
+
+# terminus connection:set "${1}.dev" git
+# STATUS=$(terminus upstream:update:status "${1}.dev")
+terminus upstream:updates:apply $DEV --updatedb -q
+SLACK="Finished ${SITE} DEV Deployment"
+curl -X POST -H 'Content-type: application/json' --data "{'text':'${SLACK}'}" $SLACK_WEBHOOK
+
+# Run drush config import, clear cache
+# terminus drush "${1}.dev" -- cim -y
+terminus env:clear-cache "${1}.dev"
+
+# Report time to results.
+DURATION=$(( SECONDS - START ))
+TIME_DIFF=$(bc <<< "scale=2; $DURATION / 60")
+MIN=$(printf "%.2f" $TIME_DIFF)
+SITE_LINK="https://live-${SITE}.pantheonsite.io";
+SLACK=":white_check_mark: Finished ${SITE} deployment in ${MIN} minutes. \n ${SITE_LINK}"
+curl -X POST -H 'Content-type: application/json' --data "{'text':'${SLACK}'}" $SLACK_WEBHOOK
+
---
+++ web/.github/scripts/test-deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Usage
+# ./live-deploy.sh <site-name or uuid>
+
+# Exit on error
+set -e
+
+SITE=$1
+DEV=$(echo "${SITE}.dev")
+START=$SECONDS
+
+# Tell slack we're starting this site
+SLACK_START="Started ${SITE} Test deployment"
+curl -X POST -H 'Content-type: application/json' --data "{'text':'${SLACK_START}'}" $SLACK_WEBHOOK
+echo -e "Starting ${SITE} Test Deployment";
+
+# Deploy code to test and live
+terminus env:deploy $TEST --cc --updatedb -n -q
+SLACK="Finished ${SITE} TEST Deployment"
+curl -X POST -H 'Content-type: application/json' --data "{'text':'${SLACK}'}" $SLACK_WEBHOOK
+
+
+# Report time to results.
+DURATION=$(( SECONDS - START ))
+TIME_DIFF=$(bc <<< "scale=2; $DURATION / 60")
+MIN=$(printf "%.2f" $TIME_DIFF)
+echo -e "Finished ${SITE} in ${MIN} minutes"
+echo "${SITE},${ID},${MIN}" >> /tmp/results.txt
+
+SITE_LINK="https://live-${SITE}.pantheonsite.io";
+SLACK=":white_check_mark: Finished ${SITE} full deployment in ${MIN} minutes. \n ${SITE_LINK}"
+curl -X POST -H 'Content-type: application/json' --data "{'text':'${SLACK}'}" $SLACK_WEBHOOK
